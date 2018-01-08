d55aebb8c7505e7cc5aba12554af5d01000968a6
Baptiste Bordet <baptiste.bordet.soat@ext.pmu.fr>
2018-01-08 18:09:27 +0100

    add server

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var data = require('./data/data.json');
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    state = {test: 'tata'};
+
+    componentDidMount() {
+        fetch('/api/test')
+            .then(res => res.json())
+            .then(test => this.setState({ test }));
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">From JSON => {data.hello}</h1>
+                    <h1 className="App-title">From server => {this.state.test}</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
