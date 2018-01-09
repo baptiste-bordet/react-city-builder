0ab16e3246edf827e6974bc31e20eb53293ad952
Baptiste Bordet <baptiste.bordet.soat@ext.pmu.fr>
2018-01-09 15:24:44 +0100

    Initial encapsulation react app by heroku project

--- server/index.js
+++ server/index.js
@@ -5,14 +5,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Priority serve any static files.
-app.use(express.static(path.resolve(__dirname, '../build')));
+app.use(express.static(path.resolve(__dirname, '../react-project/build')));
 
 // Answer API requests.
 app.get('/api/test', (req, res) => res.send({"world": "World"}));
 
 // All remaining requests return the React app, so it can handle routing.
 app.get('*', function(request, response) {
-    response.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+    response.sendFile(path.resolve(__dirname, '../react-project/build', 'index.html'));
 });
 
 app.listen(PORT, () => console.log('Server started on port ' + PORT));
--- src/server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3003;
-
-app.get('/api/test', (req, res) => res.send({"world": "World"}));
-
-app.listen(port, () => console.log('Server started on port ' + port));
