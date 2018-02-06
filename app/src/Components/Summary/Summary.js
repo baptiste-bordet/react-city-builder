30b483086aae9774db972f62e9179923ee46c326
TOSZ30-PORT-08\soat <lea.hammon@soat.fr>
2018-02-06 23:23:12 +0100

    ouvrir dans nouvel onglet

--- app/src/Components/Summary/Summary.js
+++ app/src/Components/Summary/Summary.js
@@ -12,7 +12,7 @@ class Summary extends Component {
                 <div className="">
                     <h1>En quelques lignes</h1>
 
-                    <h5>Doctorante à l'<a href="https://www.unistra.fr/index.php?id=accueil&utm_source=unistra_fr&utm_medium=unistra_fr_homepage">Université de Strasbourg</a> au laboratoire de l'<a href="https://icube.unistra.fr/">Icube</a> encadrée par  <a href="http://icube-bfo.unistra.fr/index.php/Pierre_Gan%C3%A7arski">Pierre Gançarski</a> et co-encadrée par <a href="http://irma.math.unistra.fr/~mmaumy/">Myriam Maummy-Bertrand</a> du <a href="http://irma.math.unistra.fr/">laboratoire IRMA</a>. Ma thèse est soutenue et financée par l'entreprise AB Tasty<a href="https://www.abtasty.com/fr/"/>.</h5>
+                    <h5>Doctorante à l'<a target="_blank" href="https://www.unistra.fr/index.php?id=accueil&utm_source=unistra_fr&utm_medium=unistra_fr_homepage">Université de Strasbourg</a> au laboratoire de l'<a target="_blank" href="https://icube.unistra.fr/">Icube</a> encadrée par  <a target="_blank" href="http://icube-bfo.unistra.fr/index.php/Pierre_Gan%C3%A7arski">Pierre Gançarski</a> et co-encadrée par <a target="_blank" href="http://irma.math.unistra.fr/~mmaumy/">Myriam Maummy-Bertrand</a> du <a target="_blank" href="http://irma.math.unistra.fr/">laboratoire IRMA</a>. Ma thèse est soutenue et financée par l'entreprise AB Tasty<a target="_blank" href="https://www.abtasty.com/fr/"/>.</h5>
 
                     <p>Doctorante depuis septembre 2016 mes sujets de prédilection sont:</p>
 
@@ -24,7 +24,7 @@ class Summary extends Component {
                         <li>Les systèmes de recommandation</li>
                     </ul>
 
-                    <p>Je suis également chargée de travaux dirigés à l'<a href="https://www.esiea.fr">E.S.I.E.A</a> dans le module de d'<a href="https://www.esiea.fr/wp-content/uploads/2017/12/4eAnnee.jpg">estimation et analyse de données (M1)</a>, <a href="https://www.esiea.fr/wp-content/uploads/2017/12/5eAnneeAlaCarte.jpg">d'analyse exploratoire et modèle pour la data-science (M2)</a>.</p>
+                    <p>Je suis également chargée de travaux dirigés à l'<a target="_blank" href="https://www.esiea.fr">E.S.I.E.A</a> dans le module de d'<a target="_blank" href="https://www.esiea.fr/wp-content/uploads/2017/12/4eAnnee.jpg">estimation et analyse de données (M1)</a>, <a target="_blank" href="https://www.esiea.fr/wp-content/uploads/2017/12/5eAnneeAlaCarte.jpg">d'analyse exploratoire et modèle pour la data-science (M2)</a>.</p>
 
                     <p>Attirée par la pédagogie et les statistiques, j'explore par l'informatique et les mathématiques, les secrets du mondes qui nous entoure.</p>
 
