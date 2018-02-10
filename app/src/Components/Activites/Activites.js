26f994c4baee23a6bbf04d761b11e5a249c41f51
TOSZ30-PORT-08\soat <lea.hammon@soat.fr>
2018-02-10 20:34:12 +0100

    modif padding li en mobile

--- app/src/Components/Activites/Activites.js
+++ app/src/Components/Activites/Activites.js
@@ -15,16 +15,12 @@ class Activites extends Component {
                     <p>Dans le cadre et en parallèle de ma thèse je développe plusieurs activités :</p>
 
                     <ul>
-                        <div className="col-md-6">
-                            <li><i className="fa fa-code"/>Programmation (R)</li>
-                            <li><i className="fa fa-book"/>Conseil pédagogique</li>
-                            <li><i className="fa fa-book"/>Enseignement</li>
-                        </div>
-                        <div className="col-md-6">
-                            <li><i className="fa fa-area-chart"/>Ecriture</li>
-                            <li><i className="fa fa-graduation-cap"/>Data Scientist</li>
-                            <li><i className="fa fa-users"/>Tutorat</li>
-                        </div>
+                        <li><i className="fa fa-code"/>Programmation (R)</li>
+                        <li><i className="fa fa-book"/>Conseil pédagogique</li>
+                        <li><i className="fa fa-book"/>Enseignement</li>
+                        <li><i className="fa fa-area-chart"/>Ecriture</li>
+                        <li><i className="fa fa-graduation-cap"/>Data Scientist</li>
+                        <li><i className="fa fa-users"/>Tutorat</li>
                     </ul>
 
                 </div>
